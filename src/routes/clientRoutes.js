6399bfe6f36723bb43824d831c228d858e585444
Svajunas M <mail.svajunas@gmail.com>
2022-06-19 17:35:15 +0300

    client validation

--- src/routes/clientRoutes.js
+++ src/routes/clientRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { getClient, writeClient } = require('../controllers/clientsController');
+const { validateClient } = require('../utils/helpers');
 
 const clientRoutes = express.Router();
 
 clientRoutes.get('/', getClient);
-clientRoutes.post('/add', writeClient);
+clientRoutes.post('/add', validateClient, writeClient);
 
 module.exports = clientRoutes;
