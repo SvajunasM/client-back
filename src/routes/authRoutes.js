5279342931340090ca7352ddd43148fb3f1d953f
Svajunas M <mail.svajunas@gmail.com>
2022-06-19 18:26:50 +0300

    client validation

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { authController } = require('../controllers/authController');
+const { authController, loginController } = require('../controllers/authController');
 const { validateRegistration, validateLogin } = require('../utils/helpers');
 
 const authRoutes = express.Router();
 
-authRoutes.post('/register', authController);
-authRoutes.post('/login', loginController);
+authRoutes.post('/register', validateRegistration, authController);
+authRoutes.post('/login', validateLogin, loginController);
 
 module.exports = authRoutes;
+
